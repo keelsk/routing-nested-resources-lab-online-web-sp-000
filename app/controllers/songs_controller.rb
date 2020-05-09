85a8ed0e7b9f84cfe7ad2b2b094daeed6d210526
Kirsten Keels <keelsk@gmail.com>
2020-05-09 00:19:42 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,7 @@ class SongsController < ApplicationController
   def show
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-      @song = @artist.songs.find_by(id: params[:id])
+      @song = Song.find_by(id: params[:id])
       if @song.nil?
         flash[:alert] = "Song not found."
         redirect_to artist_songs_path(@artist)
