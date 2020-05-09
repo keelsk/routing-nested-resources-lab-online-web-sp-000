7f84603f7d06a87c9201a21607c8d26a2be1a873
Kirsten Keels <keelsk@gmail.com>
2020-05-09 00:21:08 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-      if artist.nil?
+      if @artist.nil?
         flash[:alert] = "Artist not found."
         redirect_to artists_path
       else
